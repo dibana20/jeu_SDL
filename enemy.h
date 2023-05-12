e7f129036f275019eb27aec281fc78206e1d3e99
dua <127753646+dibana20@users.noreply.github.com>
2023-05-12 11:33:11 +0100

    ajout enemy.h

--- enemy.h
+++ enemy.h
@@ -10,49 +10,54 @@
 #include <unistd.h>
 
 
-
+typedef enum STATE STATE;
+enum STATE {waiting,following,attacking};
 typedef struct
 {
-  SDL_Surface *img[2][5];
-  SDL_Rect pos;
-  SDL_Surface *exp[10];
-  SDL_Rect posexp;
-  int expframe;
-  int frame;
-  int direction;
-  int arret;
+  SDL_Surface *img[4][5];  
+  SDL_Rect pos; 
+  SDL_Surface *exp[10];  
+  SDL_Rect posexp; 
+  int expframe; 
+  int frame; 
+  int direction; 
+  int arret; 
+  STATE state;
 }Enemy;
 
+
+
 typedef struct
 {
-  SDL_Rect pos2;
-  SDL_Surface *img;
+  SDL_Rect pos2; 
+  SDL_Surface *img; 
 
 }image;
 
+
 typedef struct
 {
-    SDL_Surface *sprite;
-    SDL_Surface *vies;
-    SDL_Surface *scoretxt;
-    SDL_Rect posPerso;
-    SDL_Rect posSprite;
-    SDL_Rect posVie;
-    SDL_Rect spritevie;
-    SDL_Rect posScore;
+    SDL_Surface *sprite; 
+    SDL_Surface *vies; /*!< Surface. */
+    SDL_Surface *scoretxt; /*!< Surface. */
+    SDL_Rect posPerso; /*!< Rectangle*/
+    SDL_Rect posSprite; /*!< Rectangle*/
+    SDL_Rect posVie; /*!< Rectangle*/
+    SDL_Rect spritevie; /*!< Rectangle*/
+    SDL_Rect posScore; /*!< Rectangle*/
     TTF_Font *police;
-    int direction;
-    int score;
+    int direction;  /*!< Entier */
+    int score;  /*!< Entier */
     int vie;
-    int jump_height;
-    int jump_V;
-    int up;
-    int speed;
-    float acceleration;
-    double dx;
-    SDL_Rect posRelative;
+    int jump_height;  /*!< Entier */
+    int jump_V;  /*!< Entier */
+    int up;  /*!< Entier */
+    int speed;  /*!< Entier */
+    float acceleration;  /*!< Reel */
+    double dx;  /*!< double*/
+    SDL_Rect posRelative;  /*!< Rectangle*/
     
-    SDL_Rect posAbsolu;
+    SDL_Rect posAbsolu;  /*!< Rectangle*/
 } personne;
 
 
@@ -64,14 +69,16 @@ void afficher_back(SDL_Surface *screen,image imge);
 
 
 void initEnnemi(Enemy *e);					
-void afficherEnnemi(Enemy e, SDL_Surface *screen);										
+void afficherEnnemi(Enemy e,SDL_Surface *screen);										
 void animerEnnemi(Enemy *e);					
 void move(Enemy *e);				
 int collisionBB(personne p,Enemy e); 			
-void moveIA(Enemy* e);
 void free_enemy(Enemy *e);		
 void afficher_explosion(Enemy e,SDL_Surface *screen);
 void animer_explosion(Enemy *e);
+void updateEnnemi (Enemy *e,SDL_Rect posHero);
+void moveAI(Enemy* e,SDL_Rect posHero);
+void updateEnnemiState (Enemy *e,int distEH);
 
 
 
